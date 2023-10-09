947141f331b1085bcf42b319154effe7c36a770e
noparis <nolwenn.paris@inrae.fr>
2023-10-09 16:38:02 +0200

    build : data_brenda

--- testbrendapy.py
+++ testbrendapy.py
@@ -6,7 +6,7 @@ Created on Thu Oct  5 13:53:41 2023
 @author: nparis
 """
 from collections import defaultdict
-from typing import Dict
+from typing import Dict, List
 
 path_data_brenda = '/home/nparis/brenda_enzyme/'
 file_name_json = 'brenda_2023_1.json'
@@ -20,7 +20,7 @@ from brendapy.taxonomy import Taxonomy
 BRENDA_PARSER = BrendaParser(str(path_data_brenda+file_name_txt))
 
 # =============================================================================
-# Element que nous voulons recuperer:
+# Parameters we want to retrieve from Brenda:
 # EC number
 # Km values
 # Substrat
@@ -29,30 +29,64 @@ BRENDA_PARSER = BrendaParser(str(path_data_brenda+file_name_txt))
 # Commentary
 # =============================================================================
 
+#TODO : liste des parametres
+#Dict des parametre souhaite
+# dict_set = {"EC" : None,'protein_id':None, 'organism':None, 'uniprot':None, 'KM':None}
+# def create_dict_set() -> Dict:
+#     pass
 
-# Recuperer la liste de tous les EC
-list_EC_number = BRENDA_PARSER.keys()
+def list_all_EC_in_data() -> List:
+    """
+    Gives all known EC in brenda
 
-# def parse_proteins_for_ec(ec: str = "1.1.1.1") -> Dict[int, BrendaProtein]:
-#     """Parse the protein entries for a given EC number in BRENDA."""
-#     proteins = BRENDA_PARSER.get_proteins(ec)
-#     return proteins
+    Returns
+    -------
+    List
+        list all EC number in Brenda.
+
+    """
+    return BRENDA_PARSER.keys()
 
 
-# for ec_nb in list_EC_number:
-#     parse_proteins_for_ec(ec_nb)
+def data_brenda(list_EC : list) -> List[Dict]:
+    """
+    List containing a dictionary for each protein with the parameters
+    selected
+    By selecting proteins with only the desired parameters
+    Liste contenant un dictionnaire pour chaque proteine avec les parametres
+    selectionnes
+    En selectionnant les proteines qui ont uniquement les parametres souhaite
 
-results=list()
+    BRENDA_PARSER.get_proteins returns Dict[int, BrendaProtein]
 
-p = BRENDA_PARSER.get_proteins("1.1.1.1")
-for k, v in p.items():
-    # if v.uniprot:
-    results.append({"protein_id": k,"uniprot": p.uniprot,
-                    "organism": p.organism,"KM": p.KM})
+    Parameters
+    ----------
+    list_EC : list
+        All EC number in Brenda.
 
-console.rule()
-console.print(results)
-console.rule()
+    Returns
+    -------
+    List[Dict]
+        Parameter dictionary list for each proteins.
+
+    """
+    results = list()
+    for EC_number in list_EC:
+        for k, v in BRENDA_PARSER.get_proteins(EC_number).items():
+            #TODO: facto la condition et les parametres pour pouvoir les modif
+            if v.uniprot and v.KM:
+                results.append({"EC" : EC_number, "protein_id": k,
+                                "uniprot": v.uniprot,"organism": v.organism,
+                                "KM": v.KM})
+    return results
+
+
+r = data_brenda(list_all_EC_in_data())
+
+# Affichage Brendapy
+# console.rule()
+# console.print(results)
+# console.rule()
 
 
 
