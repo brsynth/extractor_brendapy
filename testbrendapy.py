d2cf42ae8124564b8a40099ce09fa7e157a8a40f
noparis <nolwenn.paris@inrae.fr>
2023-11-23 13:52:50 +0100

    DataSetBrenda : choice ec list

--- testbrendapy.py
+++ testbrendapy.py
@@ -427,20 +427,28 @@ def parameter_sorting(list_parameter : list) -> Dict:
 
 
 class DataSetBrenda:
-    def __init__(self, list_paramater : list, path_data_brenda : str):
+    def __init__(self, list_paramater : list, path_data_brenda : str,
+                 list_ec : list = []):
         self.d_parameter_setting = parameter_sorting(list_paramater)
         #noms du fichier avec la date et heure de creation ...
         self.path_set_brend = path_data_brenda + name_new_file_created()
+
+        if list_ec:
+            self.list_ec = list_ec
+        else:
+            self.list_ec = list_all_ec_in_data()
 
     def get_cinetique_parameter(self):
         return self.d_parameter_setting
     def get_path_set_brend(self):
         return self.path_set_brend
+    def get_list_ec(self):
+        return self.list_ec
 
     #@property -> enlever () apres le run dans l'appel de fonction
     def run(self):
         create_file_json(self.get_path_set_brend(),
-                         data_brenda(list_all_ec_in_data(),
+                         data_brenda(self.get_list_ec(),
                                      self.get_cinetique_parameter()))
 
 
@@ -463,14 +471,14 @@ BRENDA_PARSER = BrendaParser(file_path_request('/home/nparis/brenda_enzyme/'))
 
 
 list_p = ["ec", "uniprot", "organism", "substrate", 'comment', 'KM', 'TN', 'value']
-# DataSetBrenda(list_p, '/home/nparis/brenda_enzyme/').run()
+DataSetBrenda(list_p, '/home/nparis/brenda_enzyme/').run()
 
 
 # for dict_proteins in BRENDA_PARSER.get_proteins('1.1.1.1').values():
 #     a = dict_proteins
 
-d_parameter_setting = parameter_sorting(list_p)
-brendaset = data_brenda(['1.1.1.10'], d_parameter_setting)
+# d_parameter_setting = parameter_sorting(list_p)
+# brendaset = data_brenda(['1.1.1.10'], d_parameter_setting)
 # brendaset = data_brenda(list_all_ec_in_data(), d_parameter_setting)
 # create_file_json(str(path_data_brenda+name_new_file_created('KM')),
 #                  data_brenda(['1.1.1.1'], 'KM'))
