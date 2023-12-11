04158120ec98a870abc98177b0ebc303cd0ab196
noparis <nolwenn.paris@inrae.fr>
2023-12-11 16:49:40 +0100

    test fct find keys with similar values

--- test_databrenda.py
+++ test_databrenda.py
@@ -18,6 +18,31 @@ class TestDataBrenda(unittest.TestCase):
     def test2_file_path_request(self):
         pass
 
+    def test_list_all_ec_in_data(self):
+        pass
+    def test_is_parameter_values(self):
+        pass
+    def test_find_shared_substrate(self):
+        pass
+    def test_d_comment_each_kinetic(self):
+        pass
+
+    def test_find__keys_with_similar_values(self):
+        d_test = {'TN': {'16': 'pH 7.0, 25°C, mutant N107D <17>'},
+                  'KM': {'20': 'pH 7.0, 25°C, mutant N107D <17>',
+                         '21': 'pH 7.0, 25°C, mutant N107L <17>'}}
+        result = [{'TN': '16', 'KM': '20'}]
+        self.assertEqual(testbrendapy.find__keys_with_similar_values(d_test), result)
+
+    def test_create_subdict_json(self):
+        pass
+    def test_create_file_json(self):
+        pass
+    def test_commun_lists(self):
+        pass
+    def test_parameter_sorting(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
