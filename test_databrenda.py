02428e26f9ba8c4c98409412bd7cfbe5cf78f740
noparis <nolwenn.paris@inrae.fr>
2023-12-11 14:37:00 +0100

    test fct file path request

--- test_databrenda.py
+++ test_databrenda.py
@@ -8,23 +8,16 @@ Created on Tue Oct 10 13:50:57 2023
 import unittest
 import testbrendapy
 
-# modifier les test pour le faire sur les plus petit fonction
 class TestDataBrenda(unittest.TestCase):
 
-    def test_good_nb_data(self):
-        setdataprot = testbrendapy.data_brenda(['1.1.1.1'], 'KM')
-        self.assertEqual(len(setdataprot), 299)
+    def test_file_path_request(self):
+        result = testbrendapy.file_path_request('/home/nparis/brenda_enzyme/',
+                                                'brenda_2023_1.txt')
+        self.assertEqual(result, '/home/nparis/brenda_enzyme/brenda_2023_1.txt')
 
-    def test_nb_organisms(self):
-        setdataprot = testbrendapy.data_brenda(['1.1.1.1'], 'KM')
-        count = 0
-        for d_prot in setdataprot:
-            if d_prot['organism'] == 'Homo sapiens':
-                count += 1
-        self.assertEqual(count,48)
+    def test2_file_path_request(self):
+        pass
 
-    def test_name_new_file_created(self):
-        self.assertEqual(testbrendapy.name_new_file_created('KKM'), 'setbrenda_KKM.json')
 
 if __name__ == '__main__':
     unittest.main()
