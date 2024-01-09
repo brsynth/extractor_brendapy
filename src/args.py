6fee055bebe688d49a06d77290be639b916dcd77
noparis <nolwenn.paris@inrae.fr>
2024-01-09 15:01:39 +0100

    dict args and fct build_args_parser and add_arguments

---
+++ src/args.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Explication
+"""
+from argparse import (
+    ArgumentParser,
+    # BooleanOptionalAction
+)
+from os import getcwd as os_getcwd
+
+from brs_utils import add_logger_args
+
+DEFAULT_ARGS = {
+    'list_parameters' : [],
+    'path_file_databrenda': '',
+}
+
+
+def build_args_parser(
+        program,
+        description):
+
+    parser = ArgumentParser(
+        program,
+        description,
+    )
+
+    parser = add_arguments(parser)
+
+    return parser
+
+
+def add_arguments(parser):
+    '''
+    
+
+    Parameters
+    ----------
+    parser : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    parser : TYPE
+        DESCRIPTION.
+
+    '''
+    parser.add_argument(
+        'list_parameters',
+        type= list,
+        help='',
+    )
+
+    parser.add_argument(
+        'path_file_databrenda',
+        type= str,
+        help='',
+    )
+    return parser
