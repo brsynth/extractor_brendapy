4e07d9e6290e52c90fc35f1255f342ce93dfcdf0
noparis <nolwenn.paris@inrae.fr>
2024-01-09 15:07:15 +0100

    import args and fct main

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: nparis
+@author: noparis
 """
+
+from .args import build_args_parser
+
+def main():
+    """
+    Main function
+    """
+    parser = build_args_parser(
+        program='',
+        description=''
+    )
+
+    args = parser.parse_args()
