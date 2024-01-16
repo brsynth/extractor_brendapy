cc0ef798e405e5fa959e3821d5e63a48143bbdae
noparis <nolwenn.paris@inrae.fr>
2024-01-16 15:46:27 +0100

    correction import

--- src/datasetbrenda.py
+++ src/datasetbrenda.py
@@ -5,20 +5,20 @@ Created on Mon Dec 11 17:10:09 2023
 
 @author: nparis
 """
-import testbrendapy
+from .testbrendapy import parameter_sorting, name_new_file_created, list_all_ec_in_data, data_brenda, create_file_json 
 # from brendapy import BrendaParser, BrendaProtein
 
 class DataSetBrenda:
     def __init__(self, list_paramater : list, path_data_brenda : str,
                  list_ec : list = []):
-        self.d_parameter_setting = testbrendapy.parameter_sorting(list_paramater)
+        self.d_parameter_setting = parameter_sorting(list_paramater)
         #noms du fichier avec la date et heure de creation ...
-        self.path_set_brend = path_data_brenda + testbrendapy.name_new_file_created()
+        self.path_set_brend = path_data_brenda + name_new_file_created()
 
         if list_ec:
             self.list_ec = list_ec
         else:
-            self.list_ec = testbrendapy.list_all_ec_in_data()
+            self.list_ec = list_all_ec_in_data()
 
     def get_cinetique_parameter(self):
         return self.d_parameter_setting
@@ -29,11 +29,11 @@ class DataSetBrenda:
 
     #@property -> enlever () apres le run dans l'appel de fonction
     def run(self):
-        testbrendapy.create_file_json(self.get_path_set_brend(),
-                         testbrendapy.data_brenda(self.get_list_ec(),
+        create_file_json(self.get_path_set_brend(),+
+                         data_brenda(self.get_list_ec(),
                                      self.get_cinetique_parameter()))
 
 
 
-# list_p = ["ec", "uniprot", "organism", "substrate", 'comment', 'KM', 'TN', 'value']
+# list_p = ["ec", "uniprot"]
 # DataSetBrenda(list_p, '/home/nparis/brenda_enzyme/').run()
