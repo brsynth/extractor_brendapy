0bd97a7bebddc19b4f94e425a0de2beebf0d5ed0
noparis <nolwenn.paris@inrae.fr>
2024-01-23 14:09:50 +0100

    script launch

--- __main__.py
+++ __main__.py
@@ -6,19 +6,20 @@
 
 import sys
 from .src.args import build_args_parser
+from .src import DataSetBrenda
 # from .datasetbrenda import DataSetBrenda
 
 
 #DataSetBrenda(list_p, '/home/nparis/brenda_enzyme/').run()
 #Mettre en premier la liste puis le chemin
-
+#Rajouter le lien ou les data genere doivnt etre depose 
 
 def main():
     """
     Main function
     """
     try:
-        opts, arg = sys.getopt.getopt(sys.argv, "hg:d", ["help", "grammar="])
+        opts, arg = sys.getopt.getopt(sys.argv, "hg:d", ["help"])
     except sys.getopt.GetoptError:
         #Si le script ne fonctionne pas
         sys.exit(2)
@@ -31,20 +32,26 @@ def main():
             global _debug
             _debug = 1
 
+    # parser = build_args_parser()
+    # args = parser.parse_args()
+
     parser = build_args_parser(
         program='',
         description=''
     )
 
     args = parser.parse_args(sys.argv[1], sys.argv[2])
+    #args.list_parameter, args.path_data_brenda
+    # DataSetBrenda(sys.argv[1], sys.argv[2]).run()
 
 if __name__ == "__main__":
     try:
         main(sys.argv[1:])
+        DataSetBrenda(sys.argv[1], sys.argv[2]).run()
     except TypeError:
         if not sys.argv[1:]:
-            list_parameter : list = input('liste des elements souhaite : ')
-            path_data_brenda : str = input('chemin vers le fichier brenda : ')
-            main(list_parameter, path_data_brenda)
+            list_parameter : list = input('List of elements : ')
+            path_data_brenda : str = input('Path to the Brenda file : ')
+            DataSetBrenda(list_parameter, path_data_brenda).run()
     finally:
         print('The End')
