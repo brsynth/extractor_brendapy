919a2ee2939913f684cffe39b4eab51e11420048
noparis <nolwenn.paris@inrae.fr>
2024-01-30 02:44:01 +0100

    lancement Datasetbrenda

--- __main__.py
+++ __main__.py
@@ -5,51 +5,43 @@
 """
 
 import sys
-from .src.args import build_args_parser
+from .src.args import add_arguments, build_args_parser
 from .src import DataSetBrenda
+from argparse import (
+    ArgumentParser,
+    Namespace
+)
+from logging import Logger
 
-#Mettre en premier la liste puis le chemin
-#Rajouter le lien ou les data genere doivnt etre depose 
 
 def main():
     """
     Main function
     """
-    try:
-        opts, arg = sys.getopt.getopt(sys.argv, "hg:d", ["help"])
-    except sys.getopt.GetoptError:
-        #Si le script ne fonctionne pas
-        sys.exit(2)
+    # try:
+    #     opts, arg = sys.getopt.getopt(sys.argv, "hg:d", ["help"])
+    # except sys.getopt.GetoptError:
+    #     #Si le script ne fonctionne pas
+    #     sys.exit(2)
     
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            # usage()
-            sys.exit()
-        elif opt == '-d':
-            global _debug
-            _debug = 1
-
-    # parser = build_args_parser()
-    # args = parser.parse_args()
-
+    # for opt, arg in opts:
+    #     if opt in ("-h", "--help"):
+    #         # usage()
+    #         sys.exit()
+    #     elif opt == '-d':
+    #         global _debug
+    #         _debug = 1
+    
     parser = build_args_parser(
-        program='',
-        description=''
+        program = 'brenda_enz_code',
+        description = '',
+        m_add_args = add_arguments
     )
-
-    args = parser.parse_args(sys.argv[1], sys.argv[2])
-    #args.list_parameter, args.path_data_brenda
-    # DataSetBrenda(sys.argv[1], sys.argv[2]).run()
+    args = parser.parse_args()
+    # print(args.list_parameters)
+    # vars(args)
+    # laisser le run tant qu'il n'y a pas @property
+    DataSetBrenda(args.list_parameters, args.path_file_databrenda).run()
 
 if __name__ == "__main__":
-    try:
-        main(sys.argv[1:])
-        DataSetBrenda(sys.argv[1:]).run()
-        #DataSetBrenda(sys.argv[1], sys.argv[2]).run()
-    except TypeError:
-        if not sys.argv[1:]:
-            list_parameter : list = input('List of elements : ')
-            path_data_brenda : str = input('Path to the Brenda file : ')
-            DataSetBrenda(list_parameter, path_data_brenda).run()
-    finally:
-        print('The End')
+    main()
