dd7fa660eb3f1d63aeb380bd78c5ce86b38b8084
noparis <nolwenn.paris@inrae.fr>
2024-03-12 16:52:09 +0100

    args list ec

--- __main__.py
+++ __main__.py
@@ -39,16 +39,9 @@ def main():
     )
     args = parser.parse_args()
 
-    #Fusionne tout
-    # joined_list = ''.join(args.list_parameters)
-    # # separe les different caractere grace au espace
-    # new_list_parameter = joined_list.split(' ')
 
-    # new_list_parameter = list(filter(lambda x: x != '', new_list_parameter))
-    # print(args.path_file_databrenda)
-    # print(new_list_parameter)
     # laisser le run tant qu'il n'y a pas @property
-    DataSetBrenda(args.list_parameters, args.path_file_databrenda).run()
+    DataSetBrenda(args.list_parameters, args.path_file_databrenda, args.list_ec).run()
     # print(args)
 
 if __name__ == "__main__":
