dc0642d79321bd4f8cf6c4d532baaf753a2fbed6
noparis <nolwenn.paris@inrae.fr>
2024-06-18 16:55:08 +0200

    debug RXN

--- RXN_CMP_file_creator.py
+++ RXN_CMP_file_creator.py
@@ -15,9 +15,9 @@ from zeep import Client
 # Ajouter au fichier susbtrat et produit
 
 
-# path = '/home/nparis/brenda_enzyme/'
-# input_file = 'test1.json'
-# file_out = 'out1.json'
+path = '/home/nparis/brenda_enzyme/'
+input_file = 'test1.json'
+file_out = 'out1.json'
 
 # =============================================================================
 # PARTIE RXN
@@ -41,6 +41,7 @@ def molecule_sep(elements: str):
     """
     eq = elements.split('+')
     result = []
+    l_molecule = {}
     
     for elet in eq:
         elet = elet.strip()
@@ -54,9 +55,10 @@ def molecule_sep(elements: str):
             else:
                 coef = int(coef)
 
+            l_molecule[molecule] = ''
             result.append((coef, molecule))
 
-    return result
+    return result, l_molecule
 
 # print(molecule_sep('monohexosylceramide + 2 ferrocytochrome b5 + O2 + 2 H+'))
 # print(molecule_sep('ATP + H2O'))
@@ -83,20 +85,28 @@ def modif_file(path : str, input_file : str, file_out : str):
     """
     with open(path + input_file, "r") as file:
         data = json.load(file)
+    CMP_data = {}
     for element in data:
         reaction_SP = element['SP_data']
         i_symbol_egale = reaction_SP.find("=")
         substrates = molecule_sep(reaction_SP[:i_symbol_egale-1])
         produits = molecule_sep(reaction_SP[i_symbol_egale+2:])
+        CMP_data.update(substrates[1])
+        CMP_data.update(produits[1])
+        # print()
+        # print(CMP_data)
         # re-extrait les ID a partir de sub et prd
         #ou
         # mol sep retourne result et la list des ID pour faire le fichier CMP
         #enregistre avec une chaine vide et c'est plus tard que je mets le smile
-        element['substrates'] = substrates
-        element['products'] = produits
+        element['substrates'] = substrates[0]
+        element['products'] = produits[0]
     
     with open(path + file_out, "w", encoding = 'utf8') as file:
         json.dump(data, file, indent = 2, ensure_ascii=False)
+    file_out2 = 'out2.json'
+    with open(path + file_out2, "w", encoding = 'utf8') as file:
+        json.dump(CMP_data, file, indent = 2, ensure_ascii=False)
 
 
 def new_filename(file: str, name : str) -> str:
@@ -129,8 +139,9 @@ def get_pubchem_cid_from_name(protein):
             return data['IdentifierList']['CID'][0]
     return None
 
-p = "ATP"
-pubchem_cid = get_pubchem_cid_from_name(p)
+# p = "ATP"
+# pubchem_cid = get_pubchem_cid_from_name(p)
+
 
 def get_molfile_from_pubchem(cid):
     url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{cid}/record/SDF"
@@ -139,14 +150,7 @@ def get_molfile_from_pubchem(cid):
         return response.text
     return None
 
-if pubchem_cid:
-    molfile_data = get_molfile_from_pubchem(pubchem_cid)
-    if molfile_data:
-        print(molfile_data)
-    else:
-        print("Erreur avec pubmed")
-else:
-    print('nope')
+# molfile_data = get_molfile_from_pubchem(pubchem_cid)
 
 
 def molfile_to_smiles2(molfile):
@@ -155,16 +159,14 @@ def molfile_to_smiles2(molfile):
         mol = Chem.MolFromMolBlock(molfile)
         if mol is None:
             raise ValueError("nope premier etape de conversion en smile")
-        
         smiles = Chem.MolToSmiles(mol)
-        
         return smiles
-    
-    except Exception as e:
-        print('e', e)
 
-smiles = molfile_to_smiles2(molfile_data)
-print(smiles)
+    except Exception as e:
+        print('le prbl est :', e)
+
+# smiles = molfile_to_smiles2(molfile_data)
+# print(smiles)
 
 
 # =============================================================================
@@ -188,3 +190,5 @@ class RXNData:
 
     def run(self):
         modif_file(self.get_path(), self.get_input_file(), self.get_file_out())
+
+RXNData(path, input_file, file_out).run()
