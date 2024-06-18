0aaf745d9ba41c6da6193fa500ac8685381b2b38
noparis <nolwenn.paris@inrae.fr>
2024-06-18 16:56:03 +0200

    test 3 molecule_sep

--- test/test_RXN_CMP.py
+++ test/test_RXN_CMP.py
@@ -7,22 +7,35 @@ import unittest
 import os
 import json
 from tempfile import TemporaryDirectory
-import RMX_file_creator as RMX
+import RXN_CMP_file_creator as RNX_CMP
 
-class TestRMXFile(unittest.TestCase):
+class TestRXNFile(unittest.TestCase):
     def test1_molecule_sep(self):
-        r = RMX.molecule_sep('monohexosylceramide + 2 ferrocytochrome b5 + O2 + 2 H+')
-        desired_r = [(1, 'monohexosylceramide'), (2, 'ferrocytochrome b5'),
-                     (1, 'O2'), (2, 'H')]
-        self.assertListEqual(r, desired_r)
+        r = RNX_CMP.molecule_sep('monohexosylceramide + 2 ferrocytochrome b5 + O2 + 2 H+')
+        desired_r = ([(1, 'monohexosylceramide'), (2, 'ferrocytochrome b5'),
+                     (1, 'O2'), (2, 'H')] , {'monohexosylceramide': '',
+                                             'ferrocytochrome b5': '', 'O2': '',
+                                             'H': ''})
+        self.assertEqual(r, desired_r)
 
     def test2_molecule_sep(self):
-        r = RMX.molecule_sep('ATP + H2O')
-        desired_r = [(1, 'ATP'), (1, 'H2O')]
-        self.assertListEqual(r, desired_r)
+        r = RNX_CMP.molecule_sep('ATP + H2O')
+        desired_r = ([(1, 'ATP'), (1, 'H2O')], {'ATP': '', 'H2O': ''})
+        self.assertEqual(r, desired_r)
     
-    def test_add_rxn_filename(self):
-        self.assertEqual('RMX_test1.json', RMX.add_rmx_filename('test1.json'))
+    def test3_molecule_sep(self):
+        r = RNX_CMP.molecule_sep('2-(3-mol)test + mol2 + (S)-2-(3-mol)test')
+        desired_r = [(1, '2-(3-mol)test'), (1, 'mol2'), (1, '(S)-2-(3-mol)test')]
+        self.assertEqual(r, desired_r)
+
+    def test1_new_filename(self):
+        self.assertEqual('RXN_test1.json', RNX_CMP.new_filename('test1.json', 'RXN'))
+
+    def test2_new_filename(self):
+        self.assertEqual('CPM_test1.json', RNX_CMP.new_filename('test1.json', 'CPM'))
+
+# class TestRXNFile(unittest.TestCase):
+#     pass
 
 if __name__ == '__main__':
     unittest.main()
